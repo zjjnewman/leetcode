eea188ca96bc22c6acedff0bdcbcf591826c9e7b
jinzhijun <jinzhijun@wondersgroup.com>
2020-01-14 12:56:48 +0800

    q1

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,16 @@
 public class Solution {
-    public int[] twoSum_1(int[] nums, int target) {
+    //    1.
+    public static int[] twoSum(int[] nums, int target) {
+        int[] indexs=new int[2];
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    indexs[0]=i;
+                    indexs[2]=j;
+                    return indexs;
+                }
+            }
+        }
         return null;
     }
 }

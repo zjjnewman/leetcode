b66a0d0f9b23d3eaa3f4bc991bb6644f6ac7b301
jin <zjjcharm@outlook.com>
2020-05-27 09:50:31 +0800

    20200527

--- src/tree/LeetCode0543.java
+++ src/tree/LeetCode0543.java
@@ -33,11 +33,6 @@ public class LeetCode0543 {
         }
 
         public int diameterOfBinaryTree2(TreeNode root) {
-            if(root == null){
-                return 0;
-            }
-            int dia1 = Math.max(treeHight(root.left) + treeHight(root.right), diameterOfBinaryTree(root.left));
-            int dia2 = diameterOfBinaryTree(root.right);
 
             Stack<TreeNode> stack = new Stack<>();
             TreeNode t = root;
@@ -58,11 +53,46 @@ public class LeetCode0543 {
         }
 
 
+        int max = 0;
+        public int diameterOfBinaryTree3(TreeNode root) {
+            depth(root);
+            return max;
+        }
+
+        public int depth(TreeNode root){
+            if(root == null){
+                return 0;
+            }
+            int left = depth(root.left);
+            int right = depth(root.right);
+            int dp = Math.max(left, right)+1;
+            max = Math.max(left + right, max);
+            return dp;
+        }
+
         public int treeHight(TreeNode root){
             if(root == null){
                 return 0;
             }
             return 1 + Math.max(treeHight(root.left), treeHight(root.right));
+        }
+    }
+
+
+    //官方题解，用时为0ms 比我15ms快了很多
+    class Solution2 {
+        int ans;
+        public int diameterOfBinaryTree(TreeNode root) {
+            ans = 1;
+            depth(root);
+            return ans - 1;
+        }
+        public int depth(TreeNode node) {
+            if (node == null) return 0; // 访问到空节点了，返回0
+            int L = depth(node.left); // 左儿子为根的子树的深度
+            int R = depth(node.right); // 右儿子为根的子树的深度
+            ans = Math.max(ans, L+R+1); // 计算d_node即L+R+1 并更新ans
+            return Math.max(L, R) + 1; // 返回该节点为根的子树的深度
         }
     }
 
